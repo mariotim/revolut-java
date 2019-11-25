4e10569c9d6e83c722486550dde8f1833c3017df
marat.timergaliev <marat.timergaliev@postcon.de>
2019-11-25 23:14:01 +0100

    concurrency testing

--- src/test/java/de/revolut/taketwo/ConcurrencyTest.java
+++ src/test/java/de/revolut/taketwo/ConcurrencyTest.java
@@ -1,32 +1,53 @@
 package de.revolut.taketwo;
 
-import org.junit.jupiter.api.BeforeAll;
+import de.revolut.taketwo.model.Balance;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
+import io.undertow.util.StatusCodes;
 import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConcurrencyTest {
-    public static final String EMAIL = "email16";
-    private static EntryPoint entryPoint;
-    private static HttpClient client;
+    private static final int POOL_SIZE = 5;
+    private static final int INVOCATION_COUNT = 1000;
+    private final String EMAIL = "emailToTestConcurrency";
+    private HttpRequestHelper httpRequestHelper;
+    private EntryPoint entryPoint;
 
-    @BeforeAll
-    static void init() throws IOException, InterruptedException {
+
+    @BeforeSuite
+    void setup() throws IOException, InterruptedException {
         entryPoint = new EntryPoint();
         entryPoint.startServer();
-        client = HttpClient.newHttpClient();
+        httpRequestHelper = new HttpRequestHelper(HttpClient.newHttpClient());
+        httpRequestHelper.verifyNewClientWithHundredBux(EMAIL);
     }
 
-    @org.testng.annotations.Test(threadPoolSize = 2,
-                                 invocationCount = 10)
+    @org.testng.annotations.Test(threadPoolSize = POOL_SIZE,
+                                 invocationCount = INVOCATION_COUNT)
     void multipleRequestsToWithdrawFromOneBankAccount() throws IOException, InterruptedException {
-//        HttpResponse<String> depositResponse = deposit(email, HUNDRED_BUX);
-//        assertThat(depositResponse.statusCode()).isEqualTo(StatusCodes.OK);
-//
-//
-//        System.out.println(withdrawResponse.body());
-        //        verifyBalance(email, new Balance(BigDecimal.ZERO));
+        HttpResponse<String> withdrawResponse = httpRequestHelper.withdraw(EMAIL,
+                                                                           new BigDecimal("0.01"));
+        assertThat(withdrawResponse.statusCode()).isEqualTo(StatusCodes.OK);
+
     }
 
+    @Test(dependsOnMethods = {"multipleRequestsToWithdrawFromOneBankAccount"})
+    void verifyBalance() throws IOException, InterruptedException {
+        BigDecimal expectedBalanceAfterAll = new BigDecimal("90.00");
+        httpRequestHelper.verifyBalance(EMAIL, new Balance(expectedBalanceAfterAll));
+    }
+
+    @AfterSuite
+    void tearDown() {
+        entryPoint.stopServer();
+    }
 }
